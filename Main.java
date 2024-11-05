7cabedc2ca23442d22594237855fa9b4c21ad7b6
codecraftlearning <codecraftlearning@gmail.com>
2024-11-05 12:39:58 +0530

    inital commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+
+        Test test = new Test();
+        System.out.println(test.sum(1,2));
+    }
+}
+
+
+
+//JDK -> JRE -> it searches for main method
+
+/*
+    class <Name of the class> {
+        public static void main (String[] args) {
+            //your code ges here
+        }
+    }
+*/
+
+
+/*
+
+    pascal case -> JavaIsAProgrammingLanguage
+    camel case -> javaIsAProgrammingLanguage
+    kebab case -> java-is-a-programming-language
+    snake case -> java_is_a_programming_language
+
+ */
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    Test() {}
+
+    protected int sum (int a, int b) {
+        return a+b;
+    }
+}
